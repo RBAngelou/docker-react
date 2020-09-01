7e08dad1cc09ca6f06500e6fd2037a53d1a54540
RB Angelou Cobosa <rb@touch-fire.com>
2020-09-02 02:11:12 +0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>What's up bitch! Nevermind</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          asdfasdfad
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
